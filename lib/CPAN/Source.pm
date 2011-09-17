8808ff86127217591056f9c7370d28f47d659922
c9s <cornelius.howl@gmail.com>
2011-09-18 02:05:39 +0800

    Add gitignore

---
+++ lib/CPAN/Source.pm
@@ -0,0 +1,32 @@
+package CPAN::Source;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+CPAN::Source -
+
+=head1 SYNOPSIS
+
+  use CPAN::Source;
+
+=head1 DESCRIPTION
+
+CPAN::Source is
+
+=head1 AUTHOR
+
+Yo-An Lin E<lt>cornelius.howl {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
